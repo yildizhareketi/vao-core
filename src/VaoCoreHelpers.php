7216281c58027cdc67efb54af091bec49eb2243b
Recai Atak <recai.atak@plusclouds.com.tr>
2022-03-06 12:39:37 +0300

    fix

--- src/VaoCoreHelpers.php
+++ src/VaoCoreHelpers.php
@@ -5,9 +5,43 @@ namespace Atak011\VaoCore;
 class VaoCoreHelpers
 {
 
-    public function bak(){
+    public function bak()
+    {
         return 'sen';
     }
+
+    public function generateActionsButton($data)
+    {
+        $str = '';
+        foreach ($data as $key => $value) {
+            if ($key == 'edit') {
+                $str .= '<a href="'.$value.'">
+                            <button type="button" class="btn btn-sm btn-warning js-tooltip-enabled" data-toggle="tooltip" title="" data-original-title="Düzenle">
+                                     <i class="fa fa-pencil-alt"></i>
+                            </button>
+                         </a>';
+            }
+            if ($key == 'show') {
+                $str .= '<a href="'.$value.'">
+                              <button type="button" class="btn btn-sm btn-success js-tooltip-enabled ml-1" data-toggle="tooltip" title="" data-original-title="Görüntüle">
+                                     <i class="fa fa-eye"></i>
+                            </button>
+                         </a>';
+            }
+            if ($key == 'delete') {
+                $str .= '<a onclick="deleteData(\''.$value.'\')">
+                            <button type="button" class="btn btn-sm btn-danger js-tooltip-enabled ml-1 deleteAction">
+                                     <i class="fa fa-trash"></i>
+                            </button>
+                         </a>';
+            }
+        }
+        return '<div class="btn-group">
+                           '.$str.'
+                        </div>';
+
+    }
+
 
 
 }
